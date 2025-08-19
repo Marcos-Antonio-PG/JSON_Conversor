c60f96f8d154adcca0b342135bddf7fc2af4bee0
Marcos Antônio de Castro Moreira <marcosantoniodecastromoreira@gmail.com>
2025-08-19 18:45:47 -0300

    Checkbox do Formulário
    Adicionado a função necessária pra aparecer os inputs 2 e 3 dos hobbies quando o checkbox estiver ativado e quando estiver desativado ele esconde os inputs.

---
+++ JSON-Conversor/script.js
@@ -0,0 +1,21 @@
+const check = document.getElementById('check')
+const inpHob1 = document.getElementById('hobbie')
+const inpHob2 = document.getElementById('hobbie2')
+const inpHob3 = document.getElementById('hobbie3')
+
+check.addEventListener('change', () => {
+    if (check.checked) {
+
+        inpHob2.classList.remove('esconder')
+        inpHob2.classList.add('mostrar')
+        inpHob3.classList.remove('esconder')
+        inpHob3.classList.add('mostrar')
+
+    } else {
+        inpHob2.classList.remove('mostrar')
+        inpHob2.classList.add('esconder')
+        inpHob3.classList.remove('mostrar')
+        inpHob3.classList.add('esconder')
+    }
+
+})
