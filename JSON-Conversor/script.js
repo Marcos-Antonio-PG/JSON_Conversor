7fd64421fe2df4c6c078e2d34cc00dc0586f1243
Marcos Antônio de Castro Moreira <marcosantoniodecastromoreira@gmail.com>
2025-08-20 01:20:45 -0300

    Finalização
    Feito o botão para converter o código para JSON e o botão que volta a converter para OBJECT.

--- JSON-Conversor/script.js
+++ JSON-Conversor/script.js
@@ -6,7 +6,8 @@ const inpNome = document.getElementById('nome')
 const inpIdade = document.getElementById('idade')
 const inpProfis = document.getElementById('profis')
 const res = document.querySelector('p.res')
-
+const jsonConvBtn = document.getElementById('json-btn')
+const stringConvBtn = document.getElementById('string-btn')
 
 check.addEventListener('change', () => {
     if (check.checked) {
@@ -33,39 +34,95 @@ function gerarObject() {
     let hobbie2 = inpHob2.value
     let hobbie3 = inpHob3.value
 
-   hobbies = []
+    hobbies = []
 
-   if (hobbie1.trim() !== '') {
+    if (hobbie1.trim() !== '') {
         hobbies.push(hobbie1)
-   }
+    }
 
-   if (check.checked & hobbie2.trim() !== '') {
+    if (check.checked & hobbie2.trim() !== '') {
         hobbies.push(hobbie2)
-   }
+    }
 
-   if (check.checked & hobbie3.trim() !== '') {
+    if (check.checked & hobbie3.trim() !== '') {
         hobbies.push(hobbie3)
-   }
+    }
 
 
-   if(nome == '') {
-    alert('Erro! Digite seu nome!')
-   } else if (idade == '') {
-    alert('Erro! Digite sua idade!')
-   } else if (profis == '') {
-    alert('Erro! Digite sua profissão, se não tiver uma, escreva "Desempregado"')
-   } else if (hobbie1 == '') {
-    alert('Erro! Digite seu Hobbie, algo que sempre faz nos tempos livres!')
-   } else {
+    if (nome == '') {
+        alert('Erro! Digite seu nome!')
+    } else if (idade == '') {
+        alert('Erro! Digite sua idade!')
+    } else if (profis == '') {
+        alert('Erro! Digite sua profissão, se não tiver uma, escreva "Desempregado"')
+    } else if (hobbie1 == '') {
+        alert('Erro! Digite seu Hobbie, algo que sempre faz nos tempos livres!')
+    } else {
 
-     res.style.padding = '10px'
-    res.innerHTML = `{
+        res.style.padding = '10px'
+        res.innerHTML = `{
     nome: "${nome}",
     idade: ${idade},
     profissão: "${profis}",
     hobbies: [${hobbies.map(h => `"${h}"`).join(", ")}],
     },`
 
-   }
-    
+    }
+
+}
+
+function converterJson() {
+
+    if (res.innerHTML == '') {
+        alert('Erro! Gere um Object para poder converter!')
+    } else {
+        let nome = inpNome.value
+        let idade = inpIdade.value
+        let profis = inpProfis.value
+        let hobbie1 = inpHob1.value
+        let hobbie2 = inpHob2.value
+        let hobbie3 = inpHob3.value
+
+        if (res.innerHTML !== '') {
+
+
+            res.style.padding = '10px'
+            res.innerHTML = `{
+            "nome": "${nome}",
+            "idade": ${idade},
+            "profissão": "${profis}",
+            "hobbies": [${hobbies.map(h => `"${h}"`).join(", ")}]
+            }`
+
+            jsonConvBtn.classList.add('esconder')
+            stringConvBtn.classList.remove('esconder')
+
+        }
+
+
+    }
+
+
+}
+
+
+function converterString() {
+    let nome = inpNome.value
+    let idade = inpIdade.value
+    let profis = inpProfis.value
+    let hobbie1 = inpHob1.value
+    let hobbie2 = inpHob2.value
+    let hobbie3 = inpHob3.value
+    let tipo = "object"
+
+    res.style.padding = '10px'
+    res.innerHTML = `{
+        nome: "${nome}",
+        idade: ${idade},
+        profissão: "${profis}",
+        hobbies: [${hobbies.map(h => `"${h}"`).join(", ")}],
+        },`
+
+        jsonConvBtn.classList.remove('esconder')
+        stringConvBtn.classList.add('esconder')
 }
